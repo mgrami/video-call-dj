4491613387c941cf0b7a76e80d58bbe59b76518c
mgrami <m.grami.m@gmail.com>
2022-08-07 01:47:46 -0700

    static settings

--- videocall/settings.py
+++ videocall/settings.py
@@ -85,16 +85,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 STATIC_URL = '/static/'
-
-# For Local Development
-STATICFILES_DIRS = [
-    BASE_DIR / "static-global",
-]
-
-# For Deployments
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / "static"
+# STATICFILES_DIRS = []
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
